19656deadb0b4f48bc9c8a3570ee01ca6be18ee4
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-06-28 13:29:22 -0400

    cleanup

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,11 +15,13 @@ const resolvers = {
     },
 
     Mutation: {
+        
         addUser: async (parent, { username, email, password }) => {
             const user = await User.create({ username, email, password });
             const token = signToken(user);
             return { token, user };
         },
+
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
             if (!user) {

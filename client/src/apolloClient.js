20cdeeb5fa25f7388000532a314798ff1c2c10f8
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-07-06 13:33:14 -0400

    further deploy

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-    uri: "http://localhost:3001/graphql"
+    uri: "http://support-guru.herokuapp.com/graphql"
 });
 
 const authLink = setContext((_, { headers }) => {

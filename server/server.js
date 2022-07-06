683fc51eff5f7accad925ce3f03f8781dea7b88e
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-07-06 13:57:52 -0400

    testing

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3001/graphql", "https://support-guru.herokuapp.com/graphql"]
+    origin: ["https://localhost:3001/graphql", "https://support-guru.herokuapp.com/graphql"]
 }));
 
 if (process.env.NODE_ENV === 'production') {
@@ -25,6 +25,8 @@ if (process.env.NODE_ENV === 'production') {
 const server = new ApolloServer({
     typeDefs,
     resolvers
+},{
+    introspection:true
 })
 
 const startApolloServer = async (typeDefs, resolvers) => {
@@ -34,7 +36,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
     db.once('open', () => {
         app.listen(PORT, () => {
             console.log(`API server running on port ${PORT}!`);
-            console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+            console.log(`Use GraphQL at https://localhost:${PORT}${server.graphqlPath}`);
         })
     })
 }

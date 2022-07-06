2a2e1d6078fab651a0f56a3ef063fb0173cc39f2
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-07-06 13:05:29 -0400

    removed options from connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/support-guru', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/support-guru');
 
 module.exports = mongoose.connection;
